bafe84fe7f943a8dc16635ebd0644e1e3e132cdb
lunaticenslaved <lunaticenslaved@yandex.ru>
2024-01-05 15:07:40 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: ['next/core-web-vitals', './node_modules/@lunaticenslaved/configs/eslint.client.js'],
+  ignorePatterns: ['dist', 'node_modules'],
+  rules: {
+    'no-console': 0,
+  },
+};
