bccb5dc179f23481050189f9a71c374b2c71bd62
lunaticenslaved <lunaticenslaved@yandex.ru>
2024-01-05 22:06:27 +0300

    feat(mail): add email verification

--- lib/mail.ts
+++ lib/mail.ts
@@ -4,6 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
+
 export async function sendTwoFactorTokenEmail(email: string, token: string) {
   await resend.emails.send({
     from: 'Acme <onboarding@resend.dev>',
