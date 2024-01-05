2cfb3efd690572c46a6731ecdc834773a76bf8be
lunaticenslaved <lunaticenslaved@yandex.ru>
2024-01-05 18:13:51 +0300

    feat(next-auth): add jwt and session callbacks

--- auth.ts
+++ auth.ts
@@ -1,7 +1,9 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import { UserRole } from '@prisma/client';
 import NextAuth from 'next-auth';
 
 import authConfig from '@/auth.config';
+import { getUserById } from '@/data/user';
 import { db } from '@/lib/db';
 
 export const {
@@ -11,6 +13,41 @@ export const {
   signOut,
 } = NextAuth({
   ...authConfig,
+  callbacks: {
+    async signIn({ user }) {
+      const existingUser = await getUserById(user.id);
+
+      if (!existingUser || !existingUser.emailVerified) {
+        return true;
+      }
+
+      return true;
+    },
+    async session({ session, token }) {
+      console.log({ sessionToken: token });
+
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
 });
